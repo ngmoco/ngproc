19c6285f1fed0ae74a8bc017f8c3fa2d39f85902
Geoff Cant <nem@erlang.geek.nz>
2011-06-09 16:03:50 -0700

    Filling out ngproc_mgr, slightly broken by transition to gl_async_bully.

--- src/ngproc_mgr.erl
+++ src/ngproc_mgr.erl
@@ -2,28 +2,35 @@
 %% @copyright Geoff Cant
 %% @author Geoff Cant <nem@erlang.geek.nz>
 %% @version {@vsn}, {@date} {@time}
-%% @doc 
+%% @doc
+%%
+%% Sync protocol:
+%%   1) Leader wins election, sends {sync, Version=1, Tag}
+%%   2) Candidates send {sync, Version=1, Tag, {node(), LocalNames}}
+%%   3) Leader integrates result, sends {synced, Version=1, Tag,
+%%   {ForeignNames, YourNames, ToBeResolved}} to each candidate
+%%
 %% @end
 %%%-------------------------------------------------------------------
 -module(ngproc_mgr).
 
--behaviour(gen_leader).
+-behaviour(gl_async_bully).
 
 -include("ng_log.hrl").
 -include("ngproc.hrl").
--include_lib("gen_leader/include/gen_leader_specs.hrl").
+-include_lib("gl_async_bully/include/gl_async_bully_specs.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 %% API
--export([start_link/0
-         ,register/3
+-export([start_link/2
+         ,register/2
          ,reregister/2
          ,unregister/1
         ]).
 
-%% gen_leader callbacks
+%% gl_async_bully callbacks
 -export([init/1,
-         elected/3,
+         elected/2,
          surrendered/3,
          handle_leader_call/4,
          handle_leader_cast/3,
@@ -31,12 +38,24 @@
          handle_call/4,
          handle_cast/3,
          handle_DOWN/3,
-         handle_info/2,
-         terminate/2,
+         handle_info/3,
+         terminate/3,
          code_change/4
         ]).
 
--record(state, {tid}).
+-record(state, {tid,
+                sync_tag,
+                resolver,
+                name}).
+
+-type sync_msg() :: {sync, {'v1',
+                            Tag::reference(),
+                            FromNode::node()},
+                     sync_msg_type()}.
+
+-type sync_msg_type() ::
+        'sync_names' |
+        {'sync_names_reply', {'v1', [ngproc:nameinfo()]}}.
 
 %%====================================================================
 %% API
@@ -46,67 +65,141 @@
 %% @doc Starts the server
 %% @end
 %%--------------------------------------------------------------------
-start_link() ->
-    CandNodes = ngproc_app:config(cluster_nodes),
-    gen_leader:start_link(?MODULE, CandNodes, [], ?MODULE, [], []).
 
-register(Name, Pid, Resolver) ->
+-spec register(ngproc:name(), pid()) ->
+                      'ok' | {'duplicate', pid()}.
+register(Name, Pid) when is_pid(Pid)->
     %% Local check for existing registration - bail out early?
-    gen_leader:leader_call(?MODULE, {register, Name, Pid, Resolver}).
+    case ngproc_lib:whereis(Name) of
+        undefined ->
+            gl_async_bully:leader_call(?MODULE, {register, Name, Pid});
+        Pid ->
+            ok;
+        OldPid when is_pid(OldPid) ->
+            {duplicate, OldPid}
+    end.
 
-reregister(Name, NewPid) ->
-    gen_leader:leader_call(?MODULE, {reregister, Name, NewPid}).
+-spec reregister(ngproc:name(), pid()) -> 'ok' | {'error', 'not_registered'}.
+reregister(Name, NewPid) when is_pid(NewPid) ->
+    gl_async_bully:leader_call(?MODULE, {reregister, Name, NewPid}).
 
+-spec unregister(ngproc:name()) -> 'ok'.
 unregister(Name) ->
-    gen_leader:leader_call(?MODULE, {unregister, Name}).
+    gl_async_bully:leader_call(?MODULE, {unregister, Name}).
+
+
+-spec start_link(Name::atom(), ngproc:resolver()) -> term().
+start_link(Name, Resolver) ->
+    CandNodes = ngproc_app:config(cluster_nodes),
+    gl_async_bully:start_link(?MODULE, CandNodes, [],
+                          ?MODULE, [Name, Resolver], []).
 
 %%====================================================================
-%% gen_leader callbacks
+%% gl_async_bully callbacks
 %%====================================================================
 
-init([]) ->
-    Tid = ets:new(?NGPROC_NAMES, [named_table, protected, ordered_set]),
-    {ok, #state{tid=Tid}}.
- 
-elected(S = #state{}, _E, _Node) ->
-    erlang:error(not_implemented),
-    {reply, all_names, S}.
+init([Name, Resolver]) ->
+    Tid = ngproc_lib:init(),
+    net_kernel:monitor_nodes(true, {nodetype, visiable}),
+    {ok, #state{tid=Tid, resolver=Resolver, name=Name}}.
 
-surrendered(S = #state{}, _Sync, _E) ->
+%% Won a complete election
+elected(S = #state{}, _E) ->
+    %% XXX - blindly overwriting old sync tag - valid?
+    SyncTag = make_ref(),
+    {ok, {sync, {v1, SyncTag, node()}, sync_names}, S#state{sync_tag=SyncTag}};
+%% Captured a new candidate
+elected(S = #state{sync_tag=SyncTag}, _E, _Node) when is_reference(SyncTag) ->
+    %% XXX - ok to just send old-ish sync tag to newly captured candidate?
+    {ok, {sync, {v1, SyncTag, node()}, sync_names}, S}.
+
+%% non-leader -- counterpart to elected
+surrendered(S = #state{},
+            {sync, {v1, SyncTag, _LeaderNode}, sync_names}, E) ->
+    leader_pid(S, E) ! sync_names_reply(SyncTag),
+    {ok, S};
+surrendered(S = #state{}, Sync, _E) ->
+    ?WARN("Unknown sync message from leader: ~p", [Sync]),
     {ok, S}.
 
-handle_leader_call({register, Name, Pid, Resolver}, _From, S = #state{}, _E) ->
-    case ngproc_lib:register(Name, Pid, Resolver) of
+%% leader ops
+handle_leader_call({register, Name, Pid}, _From, S = #state{}, _E) ->
+    case ngproc_lib:register(Name, Pid) of
         registered ->
-            {reply, ok, {register, Name, Pid, Resolver}, S};
-        {duplicate, Pid} ->
-            {reply, {duplicate, Pid}, S}
+            {reply, ok, {register, Name, Pid}, S};
+        {duplicate, CanonicalPid} ->
+            {reply,
+             {duplicate, CanonicalPid},
+             {fixup, {Name, CanonicalPid}},
+             S}
     end;
 handle_leader_call({reregister, Name, NewPid}, _From, S, _E) ->
     case ngproc_lib:reregister(Name, NewPid) of
-        reregistered ->
+        {reregistered, _OldPid} ->
             {reply, ok, {reregister, Name, NewPid}, S};
-        not_registered ->
-            {reply, not_registered, S}
+        {error, not_registered} ->
+            {reply, not_registered, S};
+        no_change ->
+            {reply, ok, S}
     end;
 handle_leader_call({unregister, Name}, _From, S, _E) ->
     case ngproc_lib:unregister(Name) of
         unregistered ->
-            {reply, ok, {unregister, Name}, S};
-        not_registered ->
-            {reply, not_registered, S}
+            {reply, ok, {unregister, Name}, S}
     end;
 handle_leader_call(Msg, _From, S, _E) ->
     ?INFO("Unexpected leader call: ~p", [Msg]),
     {noreply, S}.
 
+%% non-leader companion to handle_leader_call
+from_leader({register, Name, Pid}, S, _E) ->
+    case ngproc_lib:register(Name, Pid) of
+        registered -> ok;
+        {duplicate, OldPid} ->
+            ?ERR("This should never happen -- duplicate reg from leader.~n"
+                 "Name: ~p, pid: ~p (conflicts with ~p).",
+                 [Name, Pid, OldPid])
+    end,
+    {noreply, S};
+from_leader({reregister, Name, NewPid}, S, _E) ->
+    case ngproc_lib:reregister(Name, NewPid) of
+        no_change -> ok;
+        {reregistered, _OldPid} -> ok;
+        {error, not_registered} ->
+            ?ERR("This should never happen -- "
+                 "reregister from leader for unregistered name: ~p", [Name])
+    end,
+    {noreply, S};
+from_leader({unregister, Name}, S, _E) ->
+    ngproc_lib:unregister(Name),
+    {noreply, S};
 
-from_leader({register, _Name, _Pid, _Resolver}, _S, _E) ->
-    erlang:error(not_implemented);
-from_leader({reregister, _Name, _NewPid}, _S, _E) ->
-    erlang:error(not_implemented);
-from_leader({unregister, _Name}, _S, _E) ->
-    erlang:error(not_implemented);
+%% FIXUP
+from_leader({fixup, {Name, Pid}}, S, _E) ->
+    case ngproc_lib:reg_for(Name) of
+        not_registered ->
+            ngproc_lib:register(Name, Pid),
+            ?WARN("Applied a fixup for ~p: registered ~p",
+                  [Name, Pid]);
+        Reg = #reg{} ->
+            case ngproc_lib:reregister(Name, Pid, Reg) of
+                no_change ->
+                    ok;
+                {reregistered, OldPid} ->
+                    ?WARN("Applied a fixup for ~p: ~p now ~p",
+                          [Name, OldPid, Pid]);
+                {error, not_registered} ->
+                    ?ERR("Got re-reg error on registered name. "
+                         "Not supposed to be possible.", []),
+                    ok
+            end
+    end,
+    {noreply, S};
+
+from_leader({remove_node, Node}, S, _E) ->
+    ngproc_lib:remove_node(Node),
+    {noreply, S};
+
 from_leader(Msg, S, _E) ->
     ?ERR("Unexpected broadcast from leader: ~p", [Msg]),
     {noreply, S}.
@@ -120,13 +213,45 @@ handle_call(_Req, _From, S, _E) ->
 handle_cast(_Msg, S, _E) ->
     {noreply, S}.
 
-handle_info(_Msg, S) ->
+
+handle_info({nodedown, Node, _InfoList}, S, E) ->
+    case gl_async_bully:my_role(E) of
+        leader ->
+            ngproc_lib:remove_node(Node),
+            gl_async_bully:broadcast({from_leader,{remove_node, Node}}, E);
+        _ -> ok
+    end,
+    {noreply, S};
+
+handle_info({'DOWN', Ref, process, Pid, Reason}, S, _E) ->
+    case ngproc_lib:ref_info(Ref) of
+        {RefR, RegR} ->
+            ngproc_lib:remove(RefR, RegR),
+            {noreply, S};
+        no_such_ref ->
+            ?WARN("Got DOWN message (~p/~p)for unknown process (~p).",
+                  [Ref, Reason, Pid]),
+            {noreply, S}
+    end;
+
+handle_info({sync, {v1, SyncTag, FromNode}, Op},
+            S = #state{sync_tag=SyncTag}, _E) ->
+    handle_sync(FromNode, Op, S);
+handle_info({sync, {v1, _WrongTag, FromNode}, _Op} = SyncMsg,
+            S = #state{sync_tag=_SyncTag}, _E) ->
+    ?WARN("~p send an unrecognized sync message: ~p.~n"
+          "Morality core not installed.~n"
+          "Firing up neurotoxin emitters.",
+          [FromNode, SyncMsg]),
+    {noreply, S};
+handle_info(Msg, S, _E) ->
+    ?WARN("Unexpected message: ~p", [Msg]),
     {noreply, S}.
 
 handle_DOWN(_Node, State, _E) ->
     {ok, State}.
 
-terminate(_, _S) ->
+terminate(_, _S, _E) ->
     ok.
 
 code_change(_OldVsn, S, _E, _Extra) ->
@@ -135,3 +260,19 @@ code_change(_OldVsn, S, _E, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+leader_pid(#state{name=Name}, Election) ->
+    {Name, gl_async_bully:leader_node(Election)}.
+
+%% Leader handling sync from client
+handle_sync(FromNode, {sync_names_reply, {v1, NameData}}, S) ->
+    {_InSync,
+     Update,
+     Conflict,
+     Dropped} = SyncResult = ngproc_lib:sync_with(FromNode, NameData),
+    ?INFO("To Sync:~n~p~n", [SyncResult]),
+    {noreply, S}.
+
+sync_names_reply(SyncTag) ->
+    {sync, {v1, SyncTag, node()},
+     {sync_names_reply, ngproc_lib:local_names()}}.
